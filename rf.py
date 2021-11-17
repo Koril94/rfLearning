daf4ceaa6966149bc27f49f85216c1943fa95704
Jens <j.hauner@dotsource.de>
2021-11-17 17:03:03 +0100

    Fixed issues

--- rf.py
+++ rf.py
@@ -1,9 +1,11 @@
+import multiprocessing
 import random;
 
 numberToGuess = random.randint(1, 100)
 actions = [i for i in range(100)]
-alpha = 0.5
-gamma = 0.5
+alpha = 0.6
+gamma = 0.3
+greed = 0.75
 
 qTable = {}
 
@@ -35,11 +37,20 @@ def getMaxValue(l):
             result = index
     return max
 
+def getMinValue(l):
+    max = l[0]
+    result = 0
+    for index, value in enumerate(l):
+        if value < max:
+            max = value
+            result = index
+    return max
 
-def gameLoop(guessedNumber, min, max):
+
+def gameLoop(guessedNumber, min, max, numberToGuess):
     # guessedNumber = int(input("Versuch: "))
     if guessedNumber == numberToGuess:
-        return 100, 0, guessedNumber, min, max
+        return 10, 0, guessedNumber, min, max
     if guessedNumber < numberToGuess:
         if guessedNumber > min:
             min = guessedNumber
@@ -51,21 +62,25 @@ def gameLoop(guessedNumber, min, max):
 
 def game(episodes):
     initQTable()
+    guesses = {}
+    for i in range(100):
+        guesses[i + 1] = 0
     tries = []
     for i in range(episodes):
         numberToGuess = random.randint(1, 100)
-        reward, gameState, guessedNumber, min, max = gameLoop(random.randint(1,100), 1, 100)
+        reward, gameState, guessedNumber, min, max = gameLoop(random.randint(1,100), 1, 100, 50)
         currentTries = 0
         while gameState != 0:
             currentState = tupleToState((gameState, min, max))
            
-            currentAction = getMaxIndex(qTable[currentState])
+            currentAction = getMaxIndex(qTable[currentState]) if random.random() < greed else random.randint(0, 99)
             currentGuess = currentAction + 1
-            reward, gameState, guessedNumber, min, max = gameLoop(currentGuess, min, max)
+            guesses[currentGuess] += 1
+            reward, gameState, guessedNumber, min, max = gameLoop(currentGuess, min, max, numberToGuess)
 
             oldValue = qTable[currentState][currentAction]
-
-            maxNextValue = getMaxValue(qTable[currentState])
+            
+            maxNextValue = getMaxValue(qTable[tupleToState((gameState, min, max))]) if gameState != 0 else 200
 
             newValue = (1 - alpha) * oldValue + alpha * (reward + gamma * maxNextValue)
 
@@ -74,9 +89,25 @@ def game(episodes):
             currentTries += 1
 
         tries.append(currentTries)
-    
-    print(sum(tries) / len(tries))
+
+    # print(sum(tries) / len(tries))
+    return sum(tries) / len(tries)
+    # print(guesses)
+
+# p = multiprocessing.Pool(4)
+# # p.apply(game, args=[5000])
+
+
+for i in range(10):
+    print(game(50000))
+
+outFile = open("qTable.txt", "w")
+for row in qTable:
+    maxValue = getMaxValue(qTable[row])
+    minValue = getMinValue(qTable[row])
+    if maxValue != minValue:
+        outFile.write(str(row) + " " + str(qTable[row]) + "\n")
+outFile.close()
 
 
 
-game(5000)
