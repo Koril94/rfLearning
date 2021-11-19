da2212f5461e252c32d41f1a7d199e79384b08d6
Jens <j.hauner@dotsource.de>
2021-11-19 08:48:25 +0100

    Removed extra information for ai and added display of computer guess

--- play.py
+++ play.py
@@ -11,11 +11,17 @@ if __name__ == "__main__":
     gameState = -1
     min = 1
     max = 100
+    computerGuesses = []
     while gameState != 0:
         if playerToken == 2:
             currentState = rf.tupleToState((min, max))
             currentAction = rf.getMaxIndex(qTable[currentState])
             currentGuess = currentAction + 1
+            computerGuesses.append({
+                "min": min,
+                "max": max,
+                "guess": currentGuess
+            })
             reward, gameState, guessedNumber, min, max = rf.gameLoop(currentGuess, min, max, numberToGuess)
             if gameState == 0:
                 print("Computer won. Number was: " + str(numberToGuess))
@@ -23,7 +29,7 @@ if __name__ == "__main__":
             playerToken = 1
         if playerToken == 1:
             playerGuess = int(input("Zahl: "))
-            reward, gameState, guessedNumber, min, max = rf.gameLoop(playerGuess, min, max, numberToGuess)
+            reward, gameState, guessedNumber, test, test2 = rf.gameLoop(playerGuess, min, max, numberToGuess)
             if(gameState == 0):
                 print("Player won. Number was: " + str(numberToGuess))
                 break
@@ -33,5 +39,8 @@ if __name__ == "__main__":
             if(playerGuess > numberToGuess):
                 print("Zu hoch")
             playerToken = 2
+
+    for i, guess in enumerate(computerGuesses):
+        print(str(i + 1) + ". Versuch:" + str(guess))
 
 
